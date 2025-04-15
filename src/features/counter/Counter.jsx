6f4e9c2e1c439c0e0483688907d5a06464ec6317
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-04-15 20:26:46 +0530

    reset and inc by given
    reset button and increment input button are added and updated via store

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 import { useSelector,useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { increment, decrement,reset,incrementByAmount } from './counterSlice'
 const Counter = () => {
-    const count = useSelector((state) => state.counter.count)
-    const dispatch = useDispatch()
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState(0);
+    const addValue = Number(incrementAmount) || 0;
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
   return (
+    <section>
     <div>
         <p>Count: {count}</p>
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
+    <div>
+        <input type="number" value={incrementAmount} onChange = { (e) => setIncrementAmount(e.target.value) } />
+        <button onClick = {() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button onClick = {() => dispatch(resetAll())}>Reset</button>
+    </div>
+    </section>
+    
   )
 }
 
